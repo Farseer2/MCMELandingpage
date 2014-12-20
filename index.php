9c1572c000b02327bbe2349df9407b8440382d6d
aaldim1 <dimitribakker@gmail.com>
2014-12-20 15:17:07 +0100

    Frontend
    Needs some fixes for other devices and also needs backend. :smirk:

---
+++ index.php
@@ -0,0 +1,85 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="assets/styles/style.css">
+    </head>
+    <body>
+        <div class="navbar">
+            <ul class="nav">
+                <li><a href="#">HOME</a></li>
+                <li><a href="#">FORUMS</a></li>
+                <li><a href="#">DONATE</a></li>
+                <li><a href="#">MEDIA</a></li>
+                <li><a href="#">RESOURCES</a></li>
+                <li><a href="#">WIKI</a></li>
+                <li><a href="#">NEW PLAYERS</a></li>
+                <!--<li class="right"><a href="#">Profile</a></li>-->
+            </ul>
+        </div>
+        <div class="header">
+            <img class="logo" src="http://aaldim.tk/mcme/landingpage/v1/images/Icons/logo.png">
+            <h1 class="header1">MCME</h1>
+            <h2 class="header2">minecraft middle earth</h2>
+        </div>
+        <div class="news">
+            <div class="article">
+                <h3 class="article-header">Resource packs updated</h3>
+                <p>The following Resourcepacks have been updated to the next milestone (16 Dec 2014):
+                <ul>
+                    <li>Eriador</li>
+                    <li>Lothlorien</li>
+                    <li>Gondor</li>
+                    <li>Rohan</li>
+                </ul>
+                <a class="link" href="http://www.mcmiddleearth.com/threads/resource-packs-updated.1935/">Read More</a>
+            </div>
+            <div class="separater"></div>
+            <div class="article">
+                <h3 class="article-header">~The MCME Times~ [12/14/14]</h3>
+                <img alt="" width="500" src="http://i.imgur.com/qZRemZY.png">
+                <p><h3>Opening Statement</h3>
+It's been an interesting week for MCME with lots of things happening, both happy and sad. Projects have been finished and new ones have been started. There have been rumours about a reboot of Pelargir though that would still be a while off.
+I've seen the many, many, many suggestions you guys made. And I will take them to heart.</p>
+            <a class="link" href="http://www.mcmiddleearth.com/threads/resource-packs-updated.1935/">Read More</a>
+            </div>
+        </div>
+        <div class="sidebar">
+            <div class="button">JOIN US</div>
+            <div class="side-header">Servers</div>
+            <div class="side-content">
+                <div class="server-status">
+                    <p>Build</p><p class="online status">Online</p>
+                    <p class="ip">build.mcmiddleearth.com</p>
+                    <p>Freebuild</p><p class="online status">Online</p>
+                    <p class="ip">freebuild.mcmiddleearth.com</p>
+                    <p>PVP</p><p class="offline status">Offline</p>
+                    <p class="ip">pvp.mcmiddleearth.com</p>
+                </div>
+                <div class="side-header">Staff Online Now</div>
+                <div class="staff-list">
+                    <img class="staff-pic" src="http://www.mcmiddleearth.com/data/avatars/l/0/43.jpg?1394287711">MaDIIReD<p class="staff"></p>
+                    <img class="staff-pic" src="http://www.mcmiddleearth.com/data/avatars/l/0/5.jpg?1414188440">Ghundra<p class="staff"></p>
+                </div>
+                <div class="side-header">Jobs</div>
+                <div class="jobs">
+                    <p class="job">RoadJob1</p><p class="ip">Public</p>
+                </div>
+                <div class="side-header">Mojang</div>
+                <div clas="services">
+                    <p>WebSite</p><p class="online status">Online</p>
+                    <p class="ip">minecraft.net</p>
+                    <p>Login</p><p class="online status">Online</p>
+                    <p class="ip">Login Server</p>
+                    <p>Session</p><p class="online status">Online</p>
+                    <p class="ip">Session Server</p>
+                    <p>Skin</p><p class="online status">Online</p>
+                    <p class="ip">Skin Server</p>
+                </div>
+            </div>
+        </div>
+        <!--
+        <div class="footer">
+            <p>Home Page made by <a class="link" href="https://github.com/aaldim1/">Aaldim</a> for <a class="link" href="http://www.mcmiddleearth.com/">MCME</a>.</p>
+        </div>
+        -->
+    </body>
+</html>
